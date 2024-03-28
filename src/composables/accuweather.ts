d076c074d782c5791da83e9150a5916a5dd5138f
Jeric June Logan <loganjeric25@gmail.com>
2024-03-28 16:47:38 +0800

    removed toast

--- src/composables/accuweather.ts
+++ src/composables/accuweather.ts
@@ -1,14 +1,10 @@
 import axios from "@/lib/axios"
-import { AxiosError } from "axios"
 import { ref } from "vue"
-import { useToast } from "primevue/usetoast"
 
 /**
  * Composable for AccuWeather Forecast API Service
  */
 export default function useAccuWeather() {
-    const toast = useToast()
-
     const forecast = ref<DailyForecastResponse | null>(null)
     const forecastLoading = ref(false)
 
@@ -26,23 +22,6 @@ export default function useAccuWeather() {
             forecast.value = response.data
         } catch (error) {
             console.error(error)
-
-            let message = "Something went wrong. Please refresh the page and try again."
-
-            if (error instanceof AxiosError) {
-                message =
-                    error.message === "Network Error"
-                        ? "A network error occurred. This could be an API Key limit, CORS, or an internet connection issue."
-                        : error.response?.data.Message
-            }
-
-            toast.add({
-                severity: "error",
-                summary: "Error",
-                detail: message,
-                group: "br",
-                life: 3000,
-            })
         } finally {
             forecastLoading.value = false
         }
--- src/composables/autocomplete.ts
+++ src/composables/autocomplete.ts
@@ -1,14 +1,10 @@
 import axios from "@/lib/axios"
-import { AxiosError } from "axios"
 import { ref } from "vue"
-import { useToast } from "primevue/usetoast"
 
 /**
  * Composable for AccuWeather Location AutoComplete API Service
  */
 export default function useAutoComplete() {
-    const toast = useToast()
-
     const suggestions = ref<AccuWeatherLocation[]>([])
     const selectedLocation = ref<AccuWeatherLocation | null>(null)
 
@@ -28,23 +24,6 @@ export default function useAutoComplete() {
             suggestions.value = response.data
         } catch (error) {
             console.error(error)
-
-            let message = "Something went wrong. Please refresh the page and try again."
-
-            if (error instanceof AxiosError) {
-                message =
-                    error.message === "Network Error"
-                        ? "A network error occurred. This could be an API Key limit, CORS, or an internet connection issue."
-                        : error.response?.data.Message
-            }
-
-            toast.add({
-                severity: "error",
-                summary: "Error",
-                detail: message,
-                group: "br",
-                life: 3000,
-            })
         }
     }
 
